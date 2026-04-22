4a44b871834dbdd4de2d12ced310184d842dd352
Muskan Gupta <muskangupta79859@gmail.com>
2026-04-22 20:38:28 +0530

    Added model training module

---
+++ advance_ml/real_world_project/model_training.py
@@ -0,0 +1,12 @@
+"""
+Model Training Module
+"""
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    return model
